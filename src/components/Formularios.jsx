fb5d072e1adfd940f2048b1756e1826ea1b939c3
Neri Laredo <nerila1310@gmail.com>
2021-08-05 12:01:16 -0500

    Mostrando las opciones en el Formulario

--- src/components/Formularios.jsx
+++ src/components/Formularios.jsx
@@ -29,6 +29,12 @@ const Formulario = () => {
                         name="categoria"
                     >
                         <option value="">Selecciona Categoria</option>
+                        {categorias.map(categoria => (
+                            <option 
+                                key={categoria.strCategory}
+                                value={categoria.strCategory}
+                            >{categoria.strCategory}</option>
+                        ))}
                     </select>
                 </div>
                 <div className="col-md-4">

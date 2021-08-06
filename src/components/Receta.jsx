11e622798c3c1613c44acb514f933254329b244a
Neri Laredo <nerila1310@gmail.com>
2021-08-05 19:41:24 -0500

    Mostrando la información de las recetas

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -6,6 +6,15 @@ const Receta = ({receta}) => {
         <div className="col-md-4 mb-3">
             <div className="card">
                 <h2 className="card-header">{receta.strDrink}</h2>
+                <img className="card-img-top" src={receta.strDrinkThumb} alt={receta.strDrink}/>
+                <div className="card-body">
+                    <button
+                        type="button"
+                        className="btn btn-block btn-primary"
+                    >
+                        Ver receta
+                    </button>
+                </div>
             </div>
         </div>
     );

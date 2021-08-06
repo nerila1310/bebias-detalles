092f8029973cbefb0767d992b99c56aa126594bc
Neri Laredo <nerila1310@gmail.com>
2021-08-05 20:34:40 -0500

    Importando el componente de Modal

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,7 +1,43 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { ModalContext } from '../contexts/Modal.context';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 600,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const Receta = ({receta}) => {
+
+    //configuracion del Modal de material UI
+    const [modalstyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     //extraer los valores del context
     const {guardarIdReceta} = useContext(ModalContext);
@@ -16,11 +52,24 @@ const Receta = ({receta}) => {
                         type="button"
                         className="btn btn-block btn-primary"
                         onClick={() => {
-                            guardarIdReceta(receta.idDrink)
+                            guardarIdReceta(receta.idDrink);
+                            handleOpen();
                         }}
                     >
                         Ver receta
                     </button>
+
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            guardarIdReceta(null);
+                            handleClose();
+                        }}
+                    >
+                        <div style={modalstyle} className={classes.paper}>
+                            <h1>Desde Modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
